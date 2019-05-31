825f24a8b67ac8d833cd9f618bdbe1820a45b02c
Elkana Rop <elkana@afrikaziara.com>
2019-05-31 17:23:11 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const sgMail = require('@sendgrid/mail');
+var express = require('express')
+var app = express()
+// app.listen(3000)
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send("Direct From Dubai Email Center")
+})
+
+app.get('/signup/:receiver/:message', function (req, res) {
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+      to: req.params.receiver,
+      from: 'elkanarop@gmail.com',
+      subject: 'DFD Test Email',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+
+   sgMail.send(msg).catch( err=>{
+    console.log(error)
+  }).then( res=>{
+    res.send(res)
+  })
+})
+
